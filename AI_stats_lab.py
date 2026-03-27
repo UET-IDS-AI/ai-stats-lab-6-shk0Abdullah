df529f54637192b1f8b3be914b0d156fb4284c57
Abdullah Amjad <shkabdullah268@gmail.com>
2026-03-27 00:32:55 -0700

    Tired of doing such things

--- AI_stats_lab.py
+++ AI_stats_lab.py
@@ -25,7 +25,20 @@ def bernoulli_log_likelihood(data, theta):
     - Raise ValueError if theta is not in (0,1)
     - Raise ValueError if data contains values other than 0 and 1
     """
-    raise NotImplementedError("Implement bernoulli_log_likelihood")
+    data = np.asarray(data)
+    
+    if len(data) == 0:
+        raise ValueError("Data cannot be empty")
+    
+    if not (0 < theta < 1):
+        raise ValueError("theta must be in (0, 1)")
+    
+    if not np.all(np.isin(data, [0, 1])):
+        raise ValueError("Data must contain only 0s and 1s")
+    
+    log_likelihood = np.sum(data * np.log(theta) + (1 - data) * np.log(1 - theta))
+    
+    return log_likelihood
 
 
 def bernoulli_mle_with_comparison(data, candidate_thetas=None):
@@ -60,7 +73,37 @@ def bernoulli_mle_with_comparison(data, candidate_thetas=None):
     - Compute candidate log-likelihoods using bernoulli_log_likelihood
     - In case of ties in best candidate, return the first one encountered
     """
-    raise NotImplementedError("Implement bernoulli_mle_with_comparison")
+    data = np.asarray(data)
+    
+    if len(data) == 0:
+        raise ValueError("Data cannot be empty")
+    
+    if not np.all(np.isin(data, [0, 1])):
+        raise ValueError("Data must contain only 0s and 1s")
+    
+    if candidate_thetas is None:
+        candidate_thetas = [0.2, 0.5, 0.8]
+    
+    num_successes = np.sum(data == 1)
+    num_failures = np.sum(data == 0)
+    mle = num_successes / len(data)
+    
+    log_likelihoods = {}
+    for theta in candidate_thetas:
+        if 0 < theta < 1:
+            log_likelihoods[theta] = bernoulli_log_likelihood(data, theta)
+    
+    best_candidate = None
+    if log_likelihoods:
+        best_candidate = max(log_likelihoods.items(), key=lambda x: x[1])[0]
+    
+    return {
+        'mle': mle,
+        'num_successes': num_successes,
+        'num_failures': num_failures,
+        'log_likelihoods': log_likelihoods,
+        'best_candidate': best_candidate
+    }
 
 
 def poisson_log_likelihood(data, lam):
@@ -90,7 +133,23 @@ def poisson_log_likelihood(data, lam):
     -----
     You may use math.lgamma(x + 1) for log(x!) since log(x!) = lgamma(x+1).
     """
-    raise NotImplementedError("Implement poisson_log_likelihood")
+    data = np.asarray(data)
+    
+    if len(data) == 0:
+        raise ValueError("Data cannot be empty")
+    
+    if lam <= 0:
+        raise ValueError("lam must be > 0")
+    
+    if not np.all(data >= 0):
+        raise ValueError("Data must contain only nonnegative values")
+    
+    if not np.all(data == data.astype(int)):
+        raise ValueError("Data must contain only integer values")
+    
+    log_likelihood = np.sum(data * np.log(lam) - lam - np.array([math.lgamma(x + 1) for x in data]))
+    
+    return log_likelihood
 
 
 def poisson_mle_analysis(data, candidate_lambdas=None):
@@ -126,4 +185,39 @@ def poisson_mle_analysis(data, candidate_lambdas=None):
     - Compute candidate log-likelihoods using poisson_log_likelihood
     - In case of ties in best candidate, return the first one encountered
     """
-    raise NotImplementedError("Implement poisson_mle_analysis")
+    data = np.asarray(data)
+    
+    if len(data) == 0:
+        raise ValueError("Data cannot be empty")
+    
+    if not np.all(data >= 0):
+        raise ValueError("Data must contain only nonnegative values")
+    
+    if not np.all(data == data.astype(int)):
+        raise ValueError("Data must contain only integer values")
+    
+    if candidate_lambdas is None:
+        candidate_lambdas = [1.0, 3.0, 5.0]
+    
+    sample_mean = np.mean(data)
+    total_count = np.sum(data)
+    n = len(data)
+    mle = sample_mean
+    
+    log_likelihoods = {}
+    for lam in candidate_lambdas:
+        if lam > 0:
+            log_likelihoods[lam] = poisson_log_likelihood(data, lam)
+    
+    best_candidate = None
+    if log_likelihoods:
+        best_candidate = max(log_likelihoods.items(), key=lambda x: x[1])[0]
+    
+    return {
+        'mle': mle,
+        'sample_mean': sample_mean,
+        'total_count': total_count,
+        'n': n,
+        'log_likelihoods': log_likelihoods,
+        'best_candidate': best_candidate
+    }
